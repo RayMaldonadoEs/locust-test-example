5a71b6b4d36bca7c412966c63de502b481ce0e0f
RayMaldonadoEs <ray.maldonado@kellerhome.com>
2023-07-15 14:31:10 -0500

    Last silly commit

--- tests/locust_example.py
+++ tests/locust_example.py
@@ -1,35 +1,36 @@
-# In locust.py
+# In locust_example.py
+# Example taken from https://pflb.us/blog/load-testing-using-locust/
 
-from locust import HttpUser, task, constant
-import random
+# Requires Python 3 or higher
+
+# Running from terminal
+# locust -f ./tests/locust_example.py --headless -u 2000  -r 5  -t 60m --html report.html
+
+# does:
+# running headless request to search at kw.com - city Austin
+# Load up to 2000 users - ramp up 5 and move up
+# run for 60 minutes
+
+
+
+
+from locust import HttpUser, task, between, constant
 
 
 class QuickstartUser(HttpUser):
-    wait_time = constant(0)
+    min_wait = 5000
+    max_wait = 10000
     host = "https://kw.com"
 
-
-@task(5)
-def test_get_method(self):
-    self.client.get("search/TX/Austin?searchedText=Austin%2C%20TX%2C%20US")
-
-
-#     @task
-# def test_post_method(self):
-#     self.client.post("/articles/", {
-#         "title" : "A Title",
-#         "content" : "content!",
-#         "author_name" : "Mario",
-#     })
-#
-#     @task
-#     def test_put_method(self):
-#         self.client.put("/articles/", {
-#             "id" : "3",
-#             "title" : "Title is Updated!",
-#             "content" : f"Your lucky number: {random.randint(0,9)}!",
-#             "author_name" : "Mario 2",
-#         })
-
-def on_start(self):
-    self.client.post("/login", {"username": "foo", "password": "bar"})
+    @task(5)
+    def test_get_search_method(self):
+        resp = self.client.get("/search/TX/Austin?searchedText=Austin%2C%20TX%2C%20US")
+        print(resp.status_code)
+        print(resp.headers)
+        print(resp.request.headers)
+        # print(resp.text)  # this might be too verbose for a test load
+        
+    def on_start(self):
+        # Setup method; e.g. log in .
+        # self.client.post("/login", {"username": "foo", "password": "bar"})
+        pass
